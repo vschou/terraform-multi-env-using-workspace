3d26cf66c12018daa8dd5085b72f93f72f9ee1c3
Srikanth Vasireddy <vasireddysknth7@gmial.com>
2024-02-26 10:26:15 +0530

    added the provider script

---
+++ providers.tf
@@ -0,0 +1,76 @@
+# Specifies the provider configuration
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  alias = "dev"
+  region = var.REGION
+  profile = "dev"
+}
+
+# #######################################################################################
+# #SETUP OF PROVIDERS OF MULTI AWS ACCOUNTS
+# # SPECIFIYING THE AZURE CLOUD PROVIDER
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source = "hashicorp/azurerm"
+#       version = "=3.0.0"
+#     }
+#   }
+# }
+# # CONFIGURING AWS PROVIDERS & DIFFERENT ENV's & DIFF AWS ACCOUNTS
+# provider "aws" {
+#   alias = "dev"
+#   region = ""
+#   access_key = "" 
+#   secret_key = ""
+# }
+# provider "aws" {
+#   alias = "stage"
+#   region = ""
+#   access_key = ""
+#   secret_key = ""
+# }
+# provider "aws" {
+#   alias = "prod"
+#   region = ""
+#   access_key = ""
+#   secret_key = ""
+# }
+# #### COMMANDS TO CHECK & USE THE TERRAFORM FOR DEPLOYMENT ########
+# terraform apply -var 'provider_alias=prod' -var-file='prod.tfvars'
+# terraform plan -var 'provider_alias=prod' -var-file='prod.tfvars'
+
+# ####### CONFIGURING AZURE CLOUD PROVIDER #######
+# provider "azurerm" {
+#   alias = "dev"
+#   features {}
+#   subscription_id = ""
+#   client_id = ""
+#   client_secret = ""
+#   tenant_id = ""
+# }
+# provider "azurerm" {
+#   alias = "stage"
+#   features {}
+#   subscription_id = ""
+#   client_id = ""
+#   client_secret = ""
+#   tenant_id = ""
+# }
+# provider "azurerm" {
+#   alias = "prod"
+#   features {}
+#   subscription_id = ""
+#   client_id = ""
+#   client_secret = ""
+#   tenant_id = ""
+# }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "REGION" {
+  description = "value of REGION variable"
+}
+# variable "access_key" {
+#   description = "value of access key"
+# }
+# variable "secret_key" {
+#   description = "value of secret key"
+# }
